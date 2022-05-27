d6b23d5f530baf2614dbdab9066d6db794143bff
clock-v-spica <clock.v.spica0969@gmail.com>
2022-05-27 17:42:05 +0900

    ReplacePrefab

---
+++ Assets/Scripts/kura/HitArrow.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HitArrow : MonoBehaviour
+{
+    private Rigidbody _rb_arrow;
+
+    private AudioSource _audioSource;
+    // Start is called before the first frame update
+    void Start()
+    {
+        _rb_arrow = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Targets"))
+        {
+            _rb_arrow.constraints = RigidbodyConstraints.FreezeAll;
+            _audioSource.PlayOneShot(_audioSource.clip);
+        }
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Targets"))
+        {
+            _rb_arrow.constraints = RigidbodyConstraints.FreezeAll;
+        }
+    }
+
+}
