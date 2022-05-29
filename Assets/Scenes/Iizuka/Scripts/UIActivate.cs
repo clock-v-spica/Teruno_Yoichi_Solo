e8ff49f6ee65e600b48f60a8d70482743966bd25
Zinc0816 <zinc0816chorus@gmail.com>
2022-05-29 22:49:55 +0900

    UI_testPush
    これはテストです

---
+++ Assets/Scenes/Iizuka/Scripts/UIActivate.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIActivate : MonoBehaviour
+{
+    [SerializeField]
+    private GameObject _Shooter;
+    [SerializeField]
+    private GameObject _Receiver;
+    [SerializeField]
+    private Text _score;
+    [SerializeField]
+    private Image _hp;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(true){//_Shooter.GetComponent<>().)
+            //_hp.gameObject.SetActive(true)
+        }
+    }
+}
