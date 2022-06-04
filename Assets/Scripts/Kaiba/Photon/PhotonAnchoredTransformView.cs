6cbe106c17e1e9f715e956d22cc600da4b12d74c
Tsubasa Yoshida <kaibasyatyou11@gmail.com>
2022-06-04 22:24:04 +0900

    fix rotation.

--- Assets/Scripts/Kaiba/Photon/PhotonAnchoredTransformView.cs
+++ Assets/Scripts/Kaiba/Photon/PhotonAnchoredTransformView.cs
@@ -115,7 +115,7 @@ public class PhotonAnchoredTransformView : MonoBehaviourPun, IPunObservable
             if (this.m_SynchronizeRotation)
             {
                 this.m_NetworkRotation = (Quaternion)stream.ReceiveNext();
-                m_NetworkAnchoredRotation = Quaternion.Inverse(anchor.transform.rotation * m_NetworkRotation);
+                m_NetworkAnchoredRotation = anchor.transform.rotation * Quaternion.Inverse(m_NetworkRotation);
 
                 if (m_firstTake)
                 {
