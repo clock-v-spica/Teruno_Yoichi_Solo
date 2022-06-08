c6d76ff6e17e8ef13a2b8a5c7320fc513d8d5ce7
Tsubasa Yoshida <kaibasyatyou11@gmail.com>
2022-06-08 23:34:36 +0900

    force fixing.

--- Assets/Scripts/Kaiba/Photon/PhotonAnchoredTransformView.cs
+++ Assets/Scripts/Kaiba/Photon/PhotonAnchoredTransformView.cs
@@ -55,7 +55,7 @@ public class PhotonAnchoredTransformView : MonoBehaviourPun, IPunObservable
         if (!this.photonView.IsMine)
         {
             tr.position = Vector3.MoveTowards(tr.position, m_NetworkAnchoredPosition, this.m_Distance * (1.0f / PhotonNetwork.SerializationRate));
-            tr.rotation = Quaternion.RotateTowards(tr.rotation, m_NetworkAnchoredRotation, this.m_Angle * (1.0f / PhotonNetwork.SerializationRate));
+            tr.rotation = Quaternion.RotateTowards(tr.rotation, m_NetworkAnchoredRotation * Quaternion.Euler(0, 180, 0), this.m_Angle * (1.0f / PhotonNetwork.SerializationRate));
 
         }
     }
